22097b463fc3da8fbb23da67fb1d318d8c157a90
Martijn van Beek <martijn.vanbeek@gmail.com>
2016-05-24 19:34:26 +0200

    Devtools Docker

---
+++ version.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "→ Gulp"
+gulp -v
+echo ""
+echo "→ Grunt"
+grunt -v
+echo ""
+echo "→ Sass"
+sass -v
+echo ""
+echo "→ NPM"
+npm -v
+echo ""
+echo "→ node"
+node -v
+echo ""
+echo "→ jspm"
+jspm -v
